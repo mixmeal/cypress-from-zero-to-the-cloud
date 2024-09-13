bb2f13f5e4908b6195c646e4e5367b3728baca20
Michalis Milousis <michalismilousis@Michalis-Milousis.local>
2024-09-13 22:15:11 +0200

    cloud attempt 4

--- cypress/e2e/TAT-CSC.cy.js
+++ cypress/e2e/TAT-CSC.cy.js
@@ -248,4 +248,104 @@ describe('selects a file using a fixture to which an alias was given', () => {
       })
     })
   })
+
+  describe('clock ticking', () => {
+    it('submits the form with clock ticking', () => {
+      const someText = Cypress._.repeat('random text to repeat', 10)
+      cy.clock()
+      cy.get('#firstName')
+        .type('Test', { delay: 0 })
+      cy.get('#lastName')
+        .type('Last')
+      cy.get('#email')
+        .type('mail@test.com')
+      cy.get('#phone')
+        .type('0123123')
+      cy.get('#open-text-area')
+        .type(someText, { delay: 0 })
+      cy.contains('button', 'Send')
+        .click()
+      //cy.get('button[type="submit"]').click()
+      //cy.should('be.visible','Message successfully sent.')
+
+      cy.get('.success').should('be.visible')
+      cy.tick(3000)
+      cy.get('.success').should('not.be.visible')
+    })
+  })
+
+  describe('invoke1', () => {
+    it('displays and hides the success and error messages using .invoke()', () => {
+      const someText = Cypress._.repeat('random text to repeat', 10)
+      cy.get('.success')
+        .should('not.be.visible')
+        .invoke('show')
+        .should('be.visible')
+        .and('contain', 'Message successfully sent.')
+        .invoke('hide')
+        .should('not.be.visible')
+      cy.get('.error')
+        .should('not.be.visible')
+        .invoke('show')
+        .should('be.visible')
+        .and('contain', 'Validate the required fields!')
+        .invoke('hide')
+        .should('not.be.visible')
+    })
+  })
+
+describe('invoke2', () => {
+  it('successfully submits the form2', () => {
+    cy.get('#firstName')
+      .type('Test')
+    cy.get('#lastName')
+      .type('Last')
+    cy.get('#email')
+      .type('mail@test.com')
+    cy.get('#phone')
+      .type('0123123')
+    cy.get('#open-text-area')
+      .invoke('val','dnasd kdmasdasklmdlm')
+    cy.contains('button', 'Send')
+      //.click()
+    cy.get('#open-text-area').should('have.value','dnasd kdmasdasklmdlm')
+    })
+  })
+
+  describe('makes an HTTP request', () => {
+    it('testing API', () => {
+      cy.request({
+        method: 'GET',
+        url: 'https://tat-csc.s3.sa-east-1.amazonaws.com/index.html'})
+        .then((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.statusText).to.equal('OK')
+        expect(response.body).to.contain('TAT CSC')
+        })
+      })
+    })
+
+  describe('makes an HTTP request2', () => {
+    it('testing API', () => {
+      cy.request('https://tat-csc.s3.sa-east-1.amazonaws.com/index.html')
+        .as('getRequest')
+        .its('status')
+        .should('be.equal', 200)
+      cy.get('@getRequest')
+        .its('statusText')
+        .should('be.equal', 'OK')
+      cy.get('@getRequest')
+        .its('body')
+        .should('include', 'TAT CSC')
+        })
+      })
+
+  describe('find the #cat', () => {
+    it.only('cat', () => {
+      cy.get('#cat')
+        .invoke('show')
+        .should('be.visible')
+    })
+  })
+
 })
